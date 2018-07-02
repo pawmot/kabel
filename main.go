77dad7e1bfa3f9527700b44c58a15388a6af9cac
Paweł Motyl <pawmot@gmail.com>
2018-07-02 10:22:31 +0200

    Initial astilectron experiments

--- main.go
+++ main.go
@@ -1,141 +1,44 @@
 package main
 
 import (
+	"github.com/asticode/go-astilectron"
+	"github.com/asticode/go-astilog"
 	"flag"
-	"log"
-	"gopkg.in/AlecAivazis/survey.v1"
-	"fmt"
-	"os"
-	"gopkg.in/AlecAivazis/survey.v1/core"
-	"github.com/pawmot/kabel/dockerHandler"
-	"github.com/pawmot/kabel/sshHandler"
-	"github.com/pawmot/kabel/wiresharkHandler"
-	"github.com/pawmot/kabel/sniffer"
+	"github.com/asticode/go-astilectron-bootstrap"
+	"github.com/pkg/errors"
+)
+
+var (
+	AppName string
+	debug = flag.Bool("d", false, "enables the debug mode")
 )
 
 func main() {
-	configSurveyIconsCompat()
-
-	docker := dockerHandler.NewDockerHandler()
-	ssh := sshHandler.NewSshActor()
-	wireshark := wiresharkHandler.NewWiresharkClient()
-	sniff := sniffer.NewSnifferActor(docker, ssh, wireshark)
-
-	connect(sniff)
-	err := sniff.PullImage()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	containers := getContainers(sniff)
-	chosenName, chosenShortId := promptUserForContainer(containers)
-	ifaces := getInterfacesInContainer(sniff, chosenShortId)
-	chosenIface := promptUserForInterface(ifaces)
-
-	log.Printf("Chosen container: %s (id: %s)\n", chosenName, chosenShortId)
-	log.Printf("Chosen interface: %s\n", chosenIface)
-
-	closed, err := sniff.Sniff(chosenShortId, chosenIface)
-
-	<- closed
-
-	log.Println("Bye!")
-}
-
-func connect(s *sniffer.Actor) error {
-	var ssh = flag.String("ssh", "", "user@host of the machine that the docker daemon runs on")
-	// TODO use the port!
-	var _ = flag.Int("P", 22, "Port to use with ssh")
-	var dockerEndpoint = flag.String("endpoint", "unix:///var/run/docker.sock", "Docker endpoint to use")
 	flag.Parse()
-
-	var req sniffer.ConnectionRequest
-	if *ssh == "" {
-		req = sniffer.DirectConnectionRequest(*dockerEndpoint)
-	} else {
-		req = sniffer.TunneledConnectionRequest(*dockerEndpoint, *ssh)
+	astilog.FlagInit()
+	if err := bootstrap.Run(bootstrap.Options{
+		Asset: Asset,
+		AssetDir: AssetDir,
+		AstilectronOptions: astilectron.Options{
+			AppName: AppName,
+		},
+		Debug: *debug,
+		RestoreAssets: RestoreAssets,
+		Windows: []*bootstrap.Window{{
+			Homepage: "index.html",
+			MessageHandler: handleMessages,
+			Options: &astilectron.WindowOptions{
+				BackgroundColor: astilectron.PtrStr("#333"),
+				Center: astilectron.PtrBool(true),
+				Height: astilectron.PtrInt(600),
+				Width:  astilectron.PtrInt(600),
+			},
+		}},
+	}); err != nil {
+		astilog.Fatal(errors.Wrap(err, "running bootstrap failed"))
 	}
-
-	_, err := s.Connect(req)
-
-	return err
 }
 
-func getContainers(s *sniffer.Actor) []sniffer.Container {
-	containers, err := s.GetContainers()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(containers) == 0 {
-		fmt.Println("No containers are running, nothing to do here!")
-		os.Exit(0)
-	}
-
-	return containers
-}
-
-func promptUserForContainer(containers []sniffer.Container) (name string, id string) {
-	var names []string
-
-	for _, c := range containers {
-		names = append(names, c.Name)
-	}
-
-	chosenName := ""
-	prompt := &survey.Select{
-		Message: "Select a container to listen to:",
-		Options: names,
-	}
-	err := survey.AskOne(prompt, &chosenName, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if chosenName == "" {
-		log.Fatal("No container chosen")
-	}
-
-	var chosenShortId string
-	for _, c := range containers {
-		if c.Name == chosenName {
-			chosenShortId = c.Id[:12]
-			break
-		}
-	}
-
-	return chosenName, chosenShortId
-}
-
-func getInterfacesInContainer(s *sniffer.Actor, chosenShortId string) []string {
-	nis, err := s.GetNetworkInterfaces(chosenShortId)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var ifaces []string
-	for _, i := range nis {
-		ifaces = append(ifaces, i.Name)
-	}
-	return ifaces
-}
-
-func promptUserForInterface(ifaces []string) string {
-	chosenIface := ""
-	prompt := &survey.Select{
-		Message: "Select an interface to listen to:",
-		Options: ifaces,
-	}
-	err := survey.AskOne(prompt, &chosenIface, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return chosenIface
-}
-
-func configSurveyIconsCompat() {
-	core.ErrorIcon = "X"
-	core.HelpIcon = "????"
-	core.QuestionIcon = "?"
-	core.SelectFocusIcon = ">"
-	core.MarkedOptionIcon = "[x]"
-	core.UnmarkedOptionIcon = "[ ]"
+func handleMessages(_ *astilectron.Window, m bootstrap.MessageIn) (payload interface{}, err error) {
+	return nil, nil
 }
